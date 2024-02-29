73c701c9c68ec8feeb2b76d609b4318992300ffb
pt <2779655209@qq.com>
2024-02-29 22:43:52 +0800

    优化部分逻辑，新增剪贴板相关操作

--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,10 @@
 const http = require('http')
 const fs = require('fs')
 const request = require('request')
+const ncp = require('copy-paste')
+
+const port = 3000
+let cacheStr = ''
 
 const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain')
@@ -10,17 +14,17 @@ const server = http.createServer((req, res) => {
     data += chunk
   })
   req.on('end', () => {
-    const result = JSON.parse(data.toString())
+    const result = data && JSON.parse(data.toString())
     if (req.method === 'POST' && req.url === '/api/getFileData') {
       if (/\./.test(result.path)) {
         fs.readFile(result.path, (err, data) => {
           if (err) {
             res.statusCode = 201
-            res.end(JSON.stringify({ code: 201, err, path: result.path, msg: '本地服务读取二进制文件失败' }))
+            res.end(JSON.stringify({ code: 201, err, path: result.path, msg: '文件读取失败' }))
           } else {
             res.statusCode = 200
             if (!result.original) data = Buffer.from(data).toString('base64')
-            res.end(JSON.stringify({ code: 200, data, path: result.path, msg: '读取成功' }))
+            res.end(JSON.stringify({ code: 200, data, path: result.path, msg: '文件读取成功' }))
           }
         })
       } else {
@@ -46,6 +50,47 @@ const server = http.createServer((req, res) => {
         if (error) res.end(JSON.stringify(error))
         else res.end(JSON.stringify(JSON.parse(response.body).access_token))
       })
+    } else if (req.method === 'GET' && req.url === '/api/getClipboardUpdate') {
+      let str = ncp.paste()
+      let data
+      if (str.length < 512 && str.length !== 0) {
+        if (str !== cacheStr) {
+          // console.log('变化:' + res)
+          data = {
+            code: 200,
+            str,
+            status: 0,
+            msg: '剪切板数据改变'
+          }
+          cacheStr = str
+        } else {
+          // console.log('未变化')
+          data = {
+            code: 200,
+            str,
+            status: 1,
+            msg: '剪切板数据未改变'
+          }
+        }
+      } else if (str.length >= 512) {
+        // console.log('剪贴板内容过长')
+          data = {
+            code: 200,
+            str: '',
+            status: 2,
+            msg: '剪贴板内容过长'
+          }
+      } else {
+        // console.log('不是文本内容')
+          data = {
+            code: 200,
+            str: '',
+            status: 3,
+            msg: '不是文本内容'
+          }
+      }
+      res.statusCode = data.code
+      res.end(JSON.stringify(data))
     } else {
       res.statusCode = 404
       res.end('Not Found')
@@ -53,6 +98,6 @@ const server = http.createServer((req, res) => {
   })
 })
 
-server.listen(3000, () => {
-  console.log('Server is running on port 3000')
+server.listen(port, () => {
+  console.log('Server is running on port ' + port)
 })
